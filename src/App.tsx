f6b9ef720c8c19c747d5d69fe847ebe5b1ae7d37
emmajuliecodes <emmajuliedavidson@gmail.com>
2023-09-29 12:11:37 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<h1>It's the Final Project!</h1>
+			<div className="card">
+				<iframe
+					width="560"
+					height="315"
+					src="https://www.youtube.com/embed/9jK-NcRmVcw?si=MEG_OYkRY6G_7UUh"
+					title="YouTube video player"
+					frameBorder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					allowFullScreen></iframe>
+			</div>
+		</>
+	);
+}
+
+export default App;
