b22fc12beb1b9cbbcbbfed11f4145f6847a9e55b
kuhn-moni <kuhn.moonay@gmail.com>
2023-10-04 14:45:59 +0200

    intial commit-new branch, footer added

---
+++ src/components/Layouts/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// type Props = {}
+
+const Footer = () => {
+  return <div style={{ width: "100vw", minHeight: "200px", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "gray" }}>© Honey-Badgers-2023</div>;
+};
+
+export default Footer;
