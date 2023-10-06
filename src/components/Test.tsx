6eb165a00cf1b5f3ce1edaffe6a4144fe80b207f
Samuel Belay <samuelbel369@gmail.com>
2023-10-06 12:59:56 +0200

    commented lines removed , updated

--- src/components/Test.tsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-
-// type Props = {};
-
-// const Test = (props: Props) => {
-//   return <div>Test</div>;
-// };
-
-// export default Test;
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,6 @@
 import {createContext, useState, ReactNode, useEffect} from 'react';
 import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
-import {NotOk, User} from '../@types';
-import {useNavigate} from 'react-router-dom';
+import {User} from '../@types';
 
 //TODO: make changes to login interface and continue auth context with firebase
 
@@ -17,17 +16,6 @@ interface DefaultValue {
     avatar: File | null
   ) => Promise<void>;
 }
-
-interface RegisterResult {
-  user: User;
-  token: string;
-}
-
-// interface LoginResult {
-//   verified: boolean;
-//   token: string;
-//   user: User;
-// }
 
 const initialValue: DefaultValue = {
   user: null,
@@ -47,120 +35,23 @@ const initialValue: DefaultValue = {
 export const AuthContext = createContext<DefaultValue>(initialValue);
 
 export const AuthContextProvider = ({children}: {children: ReactNode}) => {
-  const baseURL = import.meta.env.VITE_SERVER_BASE as string;
   const [user, setUser] = useState<null | User>(null);
-  const redirect = useNavigate();
 
   const login = async (email: string, password: string) => {
     const auth = getAuth();
-    signInWithEmailAndPassword(auth, email, password)
-      //   .then((userCredential) => {
-      //     // const user = userCredential.user;
-      //     // Handle successful login here
-      //   })
-      .catch((error) => {
-        // Handle login error here
-        console.error(error);
-      });
-
-    // const myHeaders = new Headers();
-    // myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
-    // const urlencoded = new URLSearchParams();
-    // urlencoded.append('email', email);
-    // urlencoded.append('password', password);
-    // const requestOptions = {
-    //   method: 'POST',
-    //   headers: myHeaders,
-    //   body: urlencoded,
-    // };
-    // try {
-    //   const response = await fetch(`${baseURL}api/users/login`, requestOptions);
-    //   if (!response.ok) {
-    //     const result = (await response.json()) as NotOk;
-    //     alert(result.error);
-    //   } else {
-    //     const result = (await response.json()) as LoginResult;
-    //     console.log(result);
-    //     setUser(result.user);
-    //     localStorage.setItem('token', result.token);
-    //     localStorage.setItem('user', JSON.stringify(result.user));
-    //     alert('login successful!');
-    //     redirect('/');
-    //     setTimeout(() => redirect('/'), 2000);
-    //   }
-    // } catch (error) {
-    //   console.log('error', error);
-    // }
+    signInWithEmailAndPassword(auth, email, password).catch((error) => {
+      console.error(error);
+    });
   };
 
-  const register = async (
-    email: string,
-    password: string,
-    username: string,
-    avatar: File | null
-  ) => {
-    const formData = new FormData();
-    formData.append('username', username);
-    formData.append('email', email);
-    formData.append('password', password);
-    if (avatar) {
-      formData.append('image_url', avatar);
-    }
-    const requestOptions = {
-      method: 'POST',
-      body: formData,
-    };
-    try {
-      const response = await fetch(`${baseURL}api/users/new`, requestOptions);
-      if (response.ok) {
-        const result = (await response.json()) as RegisterResult;
-        const {token} = result as RegisterResult;
-        localStorage.setItem('token', token);
-        localStorage.setItem('user', JSON.stringify(result.user));
-        alert('Signup Successful, logging in...');
-        setUser(result.user);
-        setTimeout(() => redirect('/'), 2000);
-      } else {
-        const result = (await response.json()) as NotOk;
-        alert(`Something went wrong - ${result.error}`);
-      }
-    } catch (e) {
-      alert(` ${e as Error}`);
-    }
-  };
+  const register = async () => {};
 
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem('token');
-    alert('logout successful');
-    redirect('/');
-  };
+  const logout = () => {};
 
-  const getActiveUser = async () => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      try {
-        const myHeaders = new Headers();
-        myHeaders.append('Authorization', `Bearer ${token}`);
-        const requestOptions = {
-          method: 'GET',
-          headers: myHeaders,
-        };
-        const response = await fetch(`${baseURL}api/users/me`, requestOptions);
-        const result = (await response.json()) as User;
-        setUser(result);
-        console.log('active user', result);
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      setUser(null);
-    }
-  };
+  const getActiveUser = async () => {};
 
   useEffect(() => {
     getActiveUser().catch((e) => console.log(e));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
