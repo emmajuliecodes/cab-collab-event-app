21b719cfab3a6b9cbe03fb90735df5b3c64971b3
kuhn-moni <kuhn.moonay@gmail.com>
2023-10-10 16:39:00 +0200

    Formdata successfully save to firebase

--- src/components/EventModal.tsx
+++ src/components/EventModal.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { db } from "../firebaseConfig"; // replace with your path
+import { collection, addDoc } from "firebase/firestore";
 
 const EventModal: React.FC = () => {
   const [formData, setFormData] = useState({
@@ -11,17 +13,38 @@ const EventModal: React.FC = () => {
     eventName: "",
     host: "",
     image: "",
+    eventType: "public", // Default value
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    // Handle form submission here, you can access form data from formData state
-    // You can also send this data to a server or perform other actions as needed.
+
+    try {
+      const docRef = await addDoc(collection(db, "events"), formData);
+      console.log("Document written with ID: ", docRef.id);
+
+      // Optionally: Resetting form data after successful submission
+      setFormData({
+        date: "",
+        time: "",
+        attendees: "",
+        invitees: "",
+        location: "",
+        description: "",
+        eventName: "",
+        host: "",
+        image: "",
+        eventType: "public",
+      });
+    } catch (error) {
+      console.error("Error adding document: ", error);
+      // Handle the error appropriately
+    }
   };
 
   return (
@@ -32,47 +55,45 @@ const EventModal: React.FC = () => {
           <label htmlFor="date">Date:</label>
           <input type="text" id="date" name="date" value={formData.date} onChange={handleChange} />
         </div>
-
         <div>
           <label htmlFor="time">Time:</label>
           <input type="text" id="time" name="time" value={formData.time} onChange={handleChange} />
         </div>
-
         <div>
           <label htmlFor="attendees">Attendees:</label>
           <input type="text" id="attendees" name="attendees" value={formData.attendees} onChange={handleChange} />
         </div>
-
         <div>
           <label htmlFor="invitees">Invitees:</label>
           <input type="text" id="invitees" name="invitees" value={formData.invitees} onChange={handleChange} />
         </div>
-
         <div>
           <label htmlFor="location">Location:</label>
           <input type="text" id="location" name="location" value={formData.location} onChange={handleChange} />
         </div>
-
         <div>
           <label htmlFor="description">Description:</label>
           <textarea id="description" name="description" value={formData.description} onChange={handleChange} />
         </div>
-
         <div>
           <label htmlFor="eventName">Event Name:</label>
           <input type="text" id="eventName" name="eventName" value={formData.eventName} onChange={handleChange} />
         </div>
-
         <div>
           <label htmlFor="host">Host:</label>
           <input type="text" id="host" name="host" value={formData.host} onChange={handleChange} />
         </div>
-
         <div>
           <label htmlFor="image">Image:</label>
           <input type="text" id="image" name="image" value={formData.image} onChange={handleChange} />
         </div>
-
+        <div>
+          <label htmlFor="eventType">Event Type:</label>
+          <select id="eventType" name="eventType" value={formData.eventType} onChange={handleChange}>
+            <option value="public">Public</option>
+            <option value="private">Private</option>
+          </select>
+        </div>
         <button type="submit">Submit</button>
       </form>
     </div>
