113f5c77e38b12e5f690d3b273d23d24ef8bb35f
kuhn-moni <kuhn.moonay@gmail.com>
2023-10-11 21:45:02 +0200

    Firebase storage done. CORS not set up.

--- src/views/Events.tsx
+++ src/views/Events.tsx
@@ -3,48 +3,50 @@ import { collection, getDocs, query } from "firebase/firestore";
 import { Event } from "../@types";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-// // import { Link } from "react-router-dom";
 
 function AllEvents() {
-	const [eventsArray, setEventsArray] = useState<Event[]>([]);
+  const [eventsArray, setEventsArray] = useState<Event[]>([]);
 
-	useEffect(() => {
-		const fetchEvents = async () => {
-			const q = query(collection(db, "events"));
-			const querySnapshot = await getDocs(q);
-			console.log("querySnapshot", querySnapshot);
-			const eventsArray: Event[] = [];
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const q = query(collection(db, "events"));
+      const querySnapshot = await getDocs(q);
+      const eventsArray: Event[] = [];
 
-			querySnapshot.forEach((doc) => {
-				const data = doc.data() as Event;
-				eventsArray.push({ ...data });
-			});
-			setEventsArray(eventsArray);
+      querySnapshot.forEach((doc) => {
+        const data = doc.data() as Event;
+        eventsArray.push({ ...data, id: doc.id });
+      });
 
-			console.log("eventsArray", eventsArray);
-		};
-		fetchEvents().catch((e) => console.log(e));
-	}, []);
+      setEventsArray(eventsArray);
+    };
+    fetchEvents().catch((e) => console.log(e));
+  }, []);
 
-	return (
-		<>
-			<h1>Events go here...</h1>
-			<div className="event-listing">
-				{eventsArray.map((e) => {
-					return (
-						<>
-							<p>Name:{e.eventName}</p>
-							<p>Date:{e.date}</p>
-							<p>City: {e.city}</p>
-							<Link to={`/eventById/${e.id}`}>View event</Link>
-							<br></br>
-							<br></br>
-						</>
-					);
-				})}
-			</div>
-		</>
-	);
+  const imageStyle = {
+    width: "200px",
+    height: "auto",
+  };
+
+  return (
+    <>
+      <h1>Events go here...</h1>
+      <div className="event-listing">
+        {eventsArray.map((e) => {
+          return (
+            <div key={e.id} className="event-item">
+              <p>Name:{e.eventName}</p>
+              {/* Add Image Here */}
+              {e.image && <img src={e.image} alt={e.eventName} style={imageStyle} />}
+              <p>Date:{e.date}</p>
+              <p>City: {e.city}</p>
+              <Link to={`/eventById/${e.id}`}>View event</Link>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default AllEvents;
