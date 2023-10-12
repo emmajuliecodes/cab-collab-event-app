ec7dcf4e9884ea204ea01cc61a745196846534c5
emmajuliecodes <emmajuliedavidson@gmail.com>
2023-10-12 12:19:26 +0200

    remove duplicate firebase config

--- src/firebase/FireBaseConfig.ts
+++ src/firebase/FireBaseConfig.ts
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import {initializeApp} from 'firebase/app';
-import {getAuth} from 'firebase/auth';
-import {getFirestore} from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY as string,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN as string,
-  projectId: import.meta.env.VITE_PROJECTID as string,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET as string,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID as string,
-  appId: import.meta.env.VITE_APPID as string,
+	apiKey: import.meta.env.VITE_APIKEY as string,
+	authDomain: import.meta.env.VITE_AUTHDOMAIN as string,
+	projectId: import.meta.env.VITE_PROJECTID as string,
+	storageBucket: import.meta.env.VITE_STORAGEBUCKET as string,
+	messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID as string,
+	appId: import.meta.env.VITE_APPID as string,
 };
 
 // Initialize Firebase
