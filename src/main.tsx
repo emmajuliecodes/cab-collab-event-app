40ca6b3f402fdcbe83975734d088f7eba4447911
emmajuliecodes <emmajuliedavidson@gmail.com>
2023-10-12 12:50:53 +0200

    adjusting for branch merge

--- src/main.tsx
+++ src/main.tsx
@@ -1,91 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import './App.css';
+import "./index.css";
+import "./App.css";
 
-import {createBrowserRouter, RouterProvider, Outlet} from 'react-router-dom';
-
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 import { AuthContextProvider } from "./context/AuthContext.tsx";
 import Error404 from "./views/Error404.tsx";
 import Home from "./views/Home.tsx";
-import About from "./views/About.tsx";
+import FilterByCity from "./views/About.tsx";
 import Login from "./views/Login.tsx";
 import Events from "./views/Events.tsx";
 import UserProfile from "./views/Profile.tsx";
 import WithNav from "./components/Layouts/WithNav.tsx";
-import EventView from "./views/EventView.tsx";
+import EventDetailView from "./views/EventView.tsx";
 import WithFooter from "./components/Layouts/Footer/WithFooter.tsx";
 import Register from "./views/Register.tsx";
 import EventModal from "./components/EventModal.tsx";
 
-
 const router = createBrowserRouter([
-  {
-    element: (
-      <AuthContextProvider>
-        <Outlet />
-      </AuthContextProvider>
-    ),
-    // // putting context at outermost layer of router means it still wraps every route, but is also inside the router and can then use react router dom hooks like useNavigate
-    children: [
-      {
-        element: (
-          <>
-            <WithNav>
-              <Outlet />
-            </WithNav>
-            <WithFooter />x
-          </>
-        ),
-        children: [
-          {
-            path: '/',
-            element: <Home />,
-          },
-          {
-            path: '/events',
-            element: <Events />,
-          },
-          {
-            path: '/login',
-            element: <Login />,
-          },
-          {
-            path: '/eventById/:_id',
-            element: <EventView />,
-          },
+	{
+		element: (
+			<AuthContextProvider>
+				<Outlet />
+			</AuthContextProvider>
+		),
+		// // putting context at outermost layer of router means it still wraps every route, but is also inside the router and can then use react router dom hooks like useNavigate
+		children: [
+			{
+				element: (
+					<>
+						<WithNav>
+							<Outlet />
+						</WithNav>
+						<WithFooter />x
+					</>
+				),
+				children: [
+					{
+						path: "/",
+						element: <Home />,
+					},
+					{
+						path: "/events",
+						element: <Events />,
+					},
+					{
+						path: "/login",
+						element: <Login />,
+					},
+					{
+						path: "/eventById/:_id",
+						element: <EventDetailView />,
+					},
 
-          {
-            path: '/about',
-            element: <About />,
-          },
-          {
-            path: '/profile',
-            element: <UserProfile />,
-          },
-          {
-            path: '/register',
-            element: <Register />,
-          },
-          {
-            path: "/create-event",
-            element: <EventModal />,
-          },
-        ],
-      },
+					{
+						path: "/about",
+						element: <FilterByCity />,
+					},
+					{
+						path: "/profile",
+						element: <UserProfile />,
+					},
+					{
+						path: "/register",
+						element: <Register />,
+					},
+					{
+						path: "/create-event",
+						element: <EventModal />,
+					},
+				],
+			},
 
-      {
-        path: '*',
-        element: <Error404 />,
-      },
-    ],
-  },
+			{
+				path: "*",
+				element: <Error404 />,
+			},
+		],
+	},
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
