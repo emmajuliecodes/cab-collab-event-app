ec9dfedf20b8f037c80a9caa076d797e366af71c
emmajuliecodes <emmajuliedavidson@gmail.com>
2023-10-17 10:30:54 +0200

    pre merge push

--- src/components/CompleteProfileForm.tsx
+++ src/components/CompleteProfileForm.tsx
@@ -1,29 +1,47 @@
-import { useState, type FormEvent } from "react";
-type Props = {
-	title: string;
-	handleSubmit: (e: FormEvent<HTMLFormElement>, name: string) => void;
-};
+// import { useState, type FormEvent } from "react";
+// type Props = {
+// 	title: string;
+// 	handleSubmit: (e: FormEvent<HTMLFormElement>, name: string) => void;
+// };
 
-const UpdateProfile = ({ title, handleSubmit }: Props) => {
-	const [name, setName] = useState("");
 
-	return (
-		<div>
-			<h1>{title}</h1>
-			<form onSubmit={(e) => handleSubmit(e, name)}>
-				<label htmlFor="email">Email</label>
-				<input
-					id="email"
-					type="email"
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-				/>
-				<button type="submit">{title}</button>
-			</form>
-		</div>
-	);
+// const UpdateProfile = ({ title, handleSubmit }: Props) => {
+//     const [name, setName] = useState("");
+    
 
-	return;
-};
+// 	return (
+// 		<div>
+// 			<h1>{title}</h1>
+// 			<form onSubmit={(e) => handleSubmit(e, name)}>
+// 				<label htmlFor="email">Email</label>
+// 				<input
+// 					id="email"
+// 					type="email"
+// 					value={name}
+// 					onChange={(e) => setName(e.target.value)}
+// 				/>
+// 				<button type="submit">{title}</button>
+// 			</form>
+// 		</div>
+// 	);
 
-export default UpdateProfile;
+// 	return;
+// };
+
+// export default UpdateProfile;
+
+
+	// async function updateProfile() {
+	// 	try {
+		
+	// 		const id = ;
+	// 		const updateTest = doc(db, "users", id);
+
+	// 		await updateDoc(updateTest, {
+	// 			name: "Testing the test test",
+	// 		});
+	// 	} catch (e) {
+	// 		console.error("Error adding document: ", e);
+	// 	}
+	// }
+	
