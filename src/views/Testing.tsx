3b347bc49e5d4f26712a8b4a3f0c12ac6637577d
emmajuliecodes <emmajuliedavidson@gmail.com>
2023-10-19 19:57:06 +0200

    fetching list of public events

--- src/views/Testing.tsx
+++ src/views/Testing.tsx
@@ -7,128 +7,151 @@ import {
 	query,
 	where,
 } from "firebase/firestore";
-import { Event } from "../@types";
-
+import { Event, User } from "../@types";
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-// function FilterByCity() {
-// 	const [cityArray, setCityArray] = useState<(Event & { id: string })[]>([]);
-// 	const [inputValue, setInputValue] = useState("");
-
-// 	useEffect(() => {
-// 		const q = query(collection(db, "events"));
-// 		fetchByCity(q).catch((e) => console.log(e));
-// 	}, []);
-
-// 	const fetchByCity = async (q: Query<DocumentData, DocumentData>) => {
-// 		console.log(inputValue, "inputvalue");
-// 		const querySnapshot = await getDocs(q);
-// 		console.log("querySnapshot", querySnapshot);
-// 		const foundArray: (Event & { id: string })[] = querySnapshot.docs.map(
-// 			(doc) => {
-// 				const eventData = doc.data() as Event;
-// 				return { ...eventData, id: doc.id };
-// 			}
-// 		);
-
-// 		setCityArray(foundArray);
-// 		console.log(foundArray, "foundArray");
-// 	};
-
-// 	const HandleClick = () => {
-// 		const q = query(collection(db, "events"), where("city", "==", inputValue));
-// 		fetchByCity(q).catch((e) => console.log(e));
-// 	};
-
-// 	return (
-// 		<>
-// 			<h1>Testing City Filters</h1>
-
-// 			<div className="search-bar">
-// 				<input
-// 					type="text"
-// 					id="citysearch"
-// 					name="citysearch"
-// 					placeholder="Search for a city..."
-// 					onChange={(i) => {
-// 						setInputValue(i.target.value);
-// 					}}
-// 				/>
-// 				<button onClick={HandleClick}>Click me</button>
-// 			</div>
-
-// 			{cityArray.length === 0 ? (
-// 				<p>No events available for this city - why not try somewhere else?</p>
-// 			) : (
-// 				cityArray.map((e) => {
-// 					return (
-// 						<>
-// 							<p>Name: {e.eventName}</p>
-// 							<p>City: {e.city}</p>
-// 						</>
-// 					);
-// 				})
-// 			)}
-// 		</>
-// 	);
-// }
-
-function FilterPublic() {
-	const [publicArray, setPublicArray] = useState<(Event & { id: string })[]>(
-		[]
-	);
+function EventsListView() {
+	const [publicEventsArray, setPublicEventsArray] = useState<
+		(Event & { id: string })[]
+	>([]);
 
 	useEffect(() => {
-		const q = query(collection(db, "events"));
+		const q = query(
+			collection(db, "events"),
+			where("eventType", "==", "public")
+		);
 		fetchIfPublic(q).catch((e) => console.log(e));
 	}, []);
 
 	const fetchIfPublic = async (q: Query<DocumentData, DocumentData>) => {
 		const querySnapshot = await getDocs(q);
 		console.log("querySnapshot", querySnapshot);
-		const foundPublicArray: (Event & { id: string })[] = querySnapshot.docs.map(
-			(doc) => {
+		const foundPublicEventsArray: (Event & { id: string })[] =
+			querySnapshot.docs.map((doc) => {
 				const eventData = doc.data() as Event;
 				return { ...eventData, id: doc.id };
-			}
-		);
+			});
 
-		setPublicArray(foundPublicArray);
-		console.log(foundPublicArray, "foundArray");
-	};
-
-	const HandleClick = () => {
-		const q = query(
-			collection(db, "events"),
-			where("eventType", "==", "public")
-		);
-		fetchIfPublic(q).catch((e) => console.log(e));
+		setPublicEventsArray(foundPublicEventsArray);
+		console.log(foundPublicEventsArray, "foundArray");
 	};
 
 	return (
 		<>
-			<h1>Testing Public Filter</h1>
+			<h1>Events go here...</h1>
 
-			<div className="search-bar">
-				<button onClick={HandleClick}>Click me</button>
-			</div>
-			<div>
-				{publicArray.length === 0 ? (
-					<p>No events available for this city - why not try somewhere else?</p>
-				) : (
-					publicArray.map((e) => {
-						return (
-							<>
-								<p>Name: {e.eventName}</p>
-								<p>City: {e.city}</p>
-								<p>Event Type: {e.eventType}</p>
-							</>
-						);
-					})
-				)}
+			{/* My Events component here? */}
+
+			<div className="event-listing">
+				{/* if events array is zero && return p tag no events */}
+
+				{publicEventsArray.map((e) => {
+					return <EventListItem event={e} />;
+				})}
 			</div>
 		</>
 	);
 }
 
-export default FilterPublic;
+type EventListItemProps = { event: Event & { id: string } };
+
+const EventListItem: React.FC<EventListItemProps> = ({ event }) => {
+	const [userData, setUserData] = useState<User | null>(null);
+	useEffect(() => {
+		const fetchUserById = async () => {
+			const creatorUserId = event.creator_id as string;
+			const docRef = doc(db, "users", creatorUserId);
+
+			const docSnap = await getDoc(docRef);
+
+			if (docSnap.exists()) {
+				console.log("doc data", docSnap.data());
+				const data = docSnap.data() as User;
+				setUserData(data);
+			} else {
+				// setError("Something went wrong :( ");
+				console.log("No document exists");
+			}
+		};
+		fetchUserById().catch((e) => console.log(e));
+	}, []);
+
+	return (
+		<>
+			<p>Name: {event.eventName}</p>
+			<p>Date: {event.date}</p>
+			<p>City: {event.city}</p>
+			<p>Creator: {userData?.name}</p>
+			<p>Event Type: {event.eventType}</p>
+			<Link to={`/eventById/${event.id}`}>View event</Link>
+			<br></br>
+			<br></br>
+		</>
+	);
+};
+
+// Look into how to add error message if no events or fetch didn't work?
+// How to set up filter eg. by city - where to place function within here? Create hook so can be used elsewhere? Component?
+
+export default EventsListView;
+
+// function FilterPublic() {
+// 	const [publicArray, setPublicArray] = useState<(Event & { id: string })[]>(
+// 		[]
+// 	);
+
+// 	useEffect(() => {
+// 		const q = query(collection(db, "events"));
+// 		fetchIfPublic(q).catch((e) => console.log(e));
+// 	}, []);
+
+// 	const fetchIfPublic = async (q: Query<DocumentData, DocumentData>) => {
+// 		const querySnapshot = await getDocs(q);
+// 		console.log("querySnapshot", querySnapshot);
+// 		const foundPublicArray: (Event & { id: string })[] = querySnapshot.docs.map(
+// 			(doc) => {
+// 				const eventData = doc.data() as Event;
+// 				return { ...eventData, id: doc.id };
+// 			}
+// 		);
+
+// 		setPublicArray(foundPublicArray);
+// 		console.log(foundPublicArray, "foundArray");
+// 	};
+
+// 	const HandleClick = () => {
+// 		const q = query(
+// 			collection(db, "events"),
+// 			where("eventType", "==", "public")
+// 		);
+// 		fetchIfPublic(q).catch((e) => console.log(e));
+// 	};
+
+// 	return (
+// 		<>
+// 			<h1>Testing Public Filter</h1>
+
+// 			<div className="search-bar">
+// 				<button onClick={HandleClick}>Click me</button>
+// 			</div>
+// 			<div>
+// 				{publicArray.length === 0 ? (
+// 					<p>No events available for this city - why not try somewhere else?</p>
+// 				) : (
+// 					publicArray.map((e) => {
+// 						return (
+// 							<>
+// 								<p>Name: {e.eventName}</p>
+// 								<p>City: {e.city}</p>
+// 								<p>Event Type: {e.eventType}</p>
+// 							</>
+// 						);
+// 					})
+// 				)}
+// 			</div>
+// 		</>
+// 	);
+// }
+
+// export default FilterPublic;
