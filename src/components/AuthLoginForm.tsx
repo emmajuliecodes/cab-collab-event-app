d64a56a7d0811284c94b209ec081248875ef6674
Samuel Belay <samuelbel369@gmail.com>
2023-10-19 11:58:33 +0200

    merged staging into my branch, reslved conflicts

--- src/components/AuthLoginForm.tsx
+++ src/components/AuthLoginForm.tsx
@@ -1,43 +1,43 @@
-import { useState, type FormEvent } from "react";
+import {useState, type FormEvent} from 'react';
 
 type Props = {
-	title: string;
-	handleSubmit: (
-		e: FormEvent<HTMLFormElement>,
-		email: string,
-		password: string
-	) => void;
+  title: string;
+  handleSubmit: (
+    e: FormEvent<HTMLFormElement>,
+    email: string,
+    password: string
+  ) => void;
 };
 
-const AuthLoginForm = ({ title, handleSubmit }: Props) => {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+const AuthLoginForm = ({title, handleSubmit}: Props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-	// const changeHandler = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
-	return (
-		<div>
-			<h1>{title}</h1>
-			<form onSubmit={(e) => handleSubmit(e, email, password)}>
-				<label htmlFor="email">Email</label>
-				<input
-					placeholder="add your email"
-					id="email"
-					type="email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-				<label htmlFor="password">Password</label>
-				<input
-					placeholder="add your password"
-					id="password"
-					type="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<button type="submit">{title}</button>
-			</form>
-		</div>
-	);
+  // const changeHandler = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
+  return (
+    <div>
+      <h1>{title}</h1>
+      <form onSubmit={(e) => handleSubmit(e, email, password)}>
+        <label htmlFor='email'>Email</label>
+        <input
+          placeholder='add your email'
+          id='email'
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor='password'>Password</label>
+        <input
+          placeholder='add your password'
+          id='password'
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type='submit'>{title}</button>
+      </form>
+    </div>
+  );
 };
 
 export default AuthLoginForm;
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,154 +1,169 @@
-import { createContext, useState, useEffect, type FormEvent } from "react";
-import { useNavigate } from "react-router-dom";
+import {createContext, useState, useEffect, type FormEvent} from 'react';
+import {useNavigate} from 'react-router-dom';
 import {
-	type User,
-	createUserWithEmailAndPassword,
-	onAuthStateChanged,
-	signOut,
-	signInWithEmailAndPassword,
-} from "firebase/auth";
+  type User,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 import {
-	collection,
-	addDoc,
-	// updateDoc,
-	// doc,
-} from "firebase/firestore";
-import { auth, db } from "../firebase/FirebaseConfig";
-import { toast } from "react-toastify";
+  collection,
+  addDoc,
+  // updateDoc,
+  // doc,
+} from 'firebase/firestore';
+import {auth, db} from '../firebase/FirebaseConfig';
+import {toast} from 'react-toastify';
 
 interface ContextType {
-	user: User | null;
-	handleLogin: (
-		e: FormEvent<HTMLFormElement>,
-		email: string,
-		password: string
-	) => void;
-	logout: () => void;
-	handleRegister: (
-		e: FormEvent<HTMLFormElement>,
+  user: User | null;
+  handleLogin: (
+    e: FormEvent<HTMLFormElement>,
+    email: string,
+    password: string
+  ) => void;
+  logout: () => void;
+  handleRegister: (
+    e: FormEvent<HTMLFormElement>,
 
-		email: string,
-		password: string
-	) => void;
-	isChecked: boolean;
+    email: string,
+    password: string
+  ) => void;
+  isChecked: boolean;
 }
 
 const defaultValue: ContextType = {
-	user: null,
-	handleLogin: () => {
-		throw Error("No provider");
-	},
-	logout: () => {
-		throw Error("No provider");
-	},
-	handleRegister: () => {
-		throw Error("No provider");
-	},
-	isChecked: false,
+  user: null,
+  handleLogin: () => {
+    throw Error('No provider');
+  },
+  logout: () => {
+    throw Error('No provider');
+  },
+  handleRegister: () => {
+    throw Error('No provider');
+  },
+  isChecked: false,
 };
 
 export const AuthContext = createContext(defaultValue);
 
 interface Props {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const AuthContextProvider = (props: Props) => {
-	const [user, setUser] = useState<User | null>(null);
-	const [isChecked, setIsChecked] = useState(false);
-	const navigate = useNavigate();
+  const [user, setUser] = useState<User | null>(null);
+  const [isChecked, setIsChecked] = useState(false);
+  const navigate = useNavigate();
 
-	const logout = () => {
-		signOut(auth)
-			.then(() => {
-				setUser(null);
-				toast.info("logged out");
-			})
-			.catch((error) => {
-				// An error happened.
-				console.log(error);
-			});
-	};
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+        toast.info('logged out');
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error);
+      });
+  };
 
-	const handleRegister = (
-		e: FormEvent<HTMLFormElement>,
-		email: string,
-		password: string
-	) => {
-		e.preventDefault();
-		createUserWithEmailAndPassword(auth, email, password)
-			.then((userCredential) => {
-				// Signed in
-				const user = userCredential.user;
-				setUser(user);
-				console.log("new user", user);
+  // const logout = () => {
+  //   const user = auth.currentUser;
 
-				toast.success("Success, you are registered");
-				const uid = user.uid;
-				addDoc(collection(db, "users"), {
-					email: user.email,
-					uid: uid,
-					name: "",
-				});
+  //   if (user) {
+  //     signOut(auth)
+  //       .then(() => {
+  //         setUser(null);
+  //         toast.info('Logged out');
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //       });
+  //   }
+  // };
 
-				navigate("/");
-			})
-			.catch((error) => {
-				// const errorCode = error.code;
-				// const errorMessage = error.message;
-				console.log(error);
-			});
-	};
+  const handleRegister = (
+    e: FormEvent<HTMLFormElement>,
+    email: string,
+    password: string
+  ) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        setUser(user);
+        console.log('new user', user);
 
-	const handleLogin = (
-		e: FormEvent<HTMLFormElement>,
-		email: string,
-		password: string
-	) => {
-		e.preventDefault();
-		signInWithEmailAndPassword(auth, email, password)
-			.then((userCredential) => {
-				// Signed in
-				const user = userCredential.user;
-				setUser(user);
-				console.log(user);
-				toast.success("Success, you are logged in");
-				navigate("/");
-				// ...
-			})
-			.catch((error) => {
-				// const errorCode = error.code;
-				// const errorMessage = error.message;
-				console.log(error);
-			});
-	};
+        toast.success('Success, you are registered');
+        const uid = user.uid;
+        addDoc(collection(db, 'users'), {
+          email: user.email,
+          uid: uid,
+          name: '',
+        });
 
-	const checkActiveUser = () => {
-		onAuthStateChanged(auth, (user) => {
-			if (user) {
-				// User is signed in, see docs for a list of available properties
-				// https://firebase.google.com/docs/reference/js/auth.user
-				// const uid = user.uid;
-				setUser(user);
-				console.log(user, "user");
-				// ...
-			} else {
-				setUser(null);
-				// User is signed out
-				// ...
-			}
-			setIsChecked(true);
-		});
-	};
+        navigate('/');
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        console.log(error);
+      });
+  };
 
-	useEffect(() => {
-		checkActiveUser();
-	}, []);
+  const handleLogin = (
+    e: FormEvent<HTMLFormElement>,
+    email: string,
+    password: string
+  ) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        setUser(user);
+        console.log(user);
+        toast.success('Success, you are logged in');
+        navigate('/');
+        // ...
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        console.log(error);
+      });
+  };
 
-	return (
-		<AuthContext.Provider
-			value={{ user, handleLogin, logout, handleRegister, isChecked }}>
-			{props.children}
-		</AuthContext.Provider>
-	);
+  const checkActiveUser = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        // const uid = user.uid;
+        setUser(user);
+        console.log(user, 'user');
+        // ...
+      } else {
+        setUser(null);
+        // User is signed out
+        // ...
+      }
+      setIsChecked(true);
+    });
+  };
+
+  useEffect(() => {
+    checkActiveUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{user, handleLogin, logout, handleRegister, isChecked}}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
