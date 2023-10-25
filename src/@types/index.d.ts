b420d7955fe464c3ba90f22bfc8a0ca34b61cc45
kuhn-moni <kuhn.moonay@gmail.com>
2023-10-25 11:27:20 +0200

    types changed

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,38 +1,38 @@
 export interface NotOk {
-	error: string;
+  error: string;
 }
 
-export interface User {
-	_id: string;
-	name: string;
-	email: string;
-	username: string;
-	createdAt: string;
-	events: Array[];
-	avatar: string;
+export interface UserProfileData {
+  name: string;
+  phone: string;
+  email: string;
+  city: string;
+  myEvents: string[];
+  attending: string[];
+  declined: string[];
+  invites: string[];
+  uid: string;
 }
 
 export interface Event {
-	date: string;
-	time: string;
-	eventType: string;
-	attendees: Array[];
-	invitees: Array[];
-	declined: Array[];
-	pending: Array[];
-	city: string;
-	description: string;
-	eventName: string;
-	host: string | User;
-	// Location: is a type only used for window.location
-	// https://developer.mozilla.org/en-US/docs/Web/API/Location
-	// map_location: Location;
-	street_address: string;
-	image: string;
-	public: boolean;
-	creator_id: string;
+  date: string;
+  time: string;
+  eventType: string;
+  attendees: Array[];
+  invitees: Array[];
+  declined: Array[];
+  pending: Array[];
+  city: string;
+  description: string;
+  eventName: string;
+  host: string | User;
+  // Location: is a type only used for window.location
+  // https://developer.mozilla.org/en-US/docs/Web/API/Location
+  // map_location: Location;
+  street_address: string;
+  image: string;
+  public: boolean;
+  creator_id: string;
 }
-
-// public or private - true fales - use for My Events
 
 export type Users = User[];
